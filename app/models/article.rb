347bd94d62b3222cf6aac8e3eaf62592b52e487f
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-05-02 14:56:09 -0400

    Using custom validations and callbacks

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,17 +1,22 @@
 class Article < ActiveRecord::Base
   validates :title, :presence => true
   validates :body, :presence => true
-
+  validates :user_id, :presence => true
+ 
   belongs_to :user
   has_and_belongs_to_many :categories
   has_many :comments
-
+ 
   scope :published, where("articles.published_at IS NOT NULL")
   scope :draft, where("articles.published_at IS NULL")
   scope :recent, lambda { published.where("articles.published_at > ?", 1.week.ago.to_date)}
   scope :where_title, lambda { |term| where("articles.title LIKE ?", "%#{term}%") }
-
+ 
   def long_title
     "#{title} - #{published_at}"
+  end
+  
+  def published?
+    published_at.present?
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  
+  validate :article_should_be_published
+  
+  after_create :email_article_author
+
+  def article_should_be_published
+    errors.add(:article_id, "is not published yet") if article && !article.published?
+  end  
+
+  def email_article_author
+    puts "We will notify the author in Chapter 9"
+  end
 end
