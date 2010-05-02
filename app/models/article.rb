ebe9680e24a2ce407ec7b0f2a41c22e942ca378e
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-05-02 16:06:05 -0400

    No user_id validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, :presence => true
   validates :body, :presence => true
-  validates :user_id, :presence => true
  
   belongs_to :user
   has_and_belongs_to_many :categories
