18ca3037c25a3b9f7e2cfa3e483bfa8df8d69324
rbarazi <rbarazi@gmail.com>
2010-05-03 16:20:17 -0400

    Added owned_by? method

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,4 +18,9 @@ class Article < ActiveRecord::Base
   def published?
     published_at.present?
   end
+  
+  def owned_by?(owner)
+    return false unless owner.is_a? User
+    user == owner
+  end
 end
