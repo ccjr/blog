583da15abfd94b1c773dca39d7fb893f69d7cd37
rbarazi <rbarazi@gmail.com>
2010-05-03 15:08:12 -0400

    Adding the session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    if user = User.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, :notice => "Logged in successfully"
+    else
+      flash.now[:alert] = "Invalid login/password combination"
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, :notice => "You successfully logged out"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Blog::Application.routes.draw do |map|
     resources :comments
   end
   resources :users
+  resource :session
+  match '/login' => "sessions#new", :as => "login"
+  match '/logout' => "sessions#destroy", :as => "logout"  
 end
