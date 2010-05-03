70a455ce84480fd10c3d611ff117a7ae826e729d
rbarazi <rbarazi@gmail.com>
2010-05-03 14:31:36 -0400

    Add sessions table and configure the session store to be active record

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100502190439) do
+ActiveRecord::Schema.define(:version => 20100503182756) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(:version => 20100502190439) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email"
