9dadb9b204fe44fe8b3392230ee5a30850ce26d1
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-05-05 11:48:16 -0400

    Sending plain text email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,14 +1,10 @@
 class Notifier < ActionMailer::Base
   default :from => "from@example.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.actionmailer.notifier.email_friend.subject
-  #
-  def email_friend
-    @greeting = "Hi"
+  def email_friend(article, sender_name, receiver_email)
+    @article = article
+    @sender_name = sender_name
 
-    mail :to => "to@example.org"
+    mail :to => receiver_email, :subject => "Interesting Article"
   end
 end
