e4ebc6f9ebd89e0aa32bf9b9b75059397da156a5
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-05-05 14:58:14 -0400

    Testing article crud

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should create article" do
+    article = Article.new
+
+    article.user  = users(:eugene)
+    article.title = "Test article"
+    article.body  = "Test body"
+
+    assert article.save
+  end
+
+  test "should find article" do
+    article_id = articles(:welcome_to_rails).id
+    assert_nothing_raised { Article.find(article_id) }
+  end
+
+  test "should update article" do
+    article = articles(:welcome_to_rails)
+    assert article.update_attributes(:title => 'New title')
+  end
+
+  test "should destroy article" do
+    article = articles(:welcome_to_rails)
+    article.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Article.find(article.id) }
   end
 end
