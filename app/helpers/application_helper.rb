b45f10bc8e8585f0c993e79a526f4dae2a339ae3
rbarazi <rbarazi@gmail.com>
2010-05-05 17:43:15 -0400

    I18n application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Creates a submit button with the given name with a cancel link
   # Accepts two arguments: Form object and the cancel link name
-  def submit_or_cancel(form, name='Cancel')
-    form.submit + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+  def submit_or_cancel(form, name=t('general.cancel'))
+    form.submit + " #{t('general.or')} " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
   end
 end
