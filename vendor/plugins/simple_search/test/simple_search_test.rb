e5ff81a24008081bb5d0a7ebaa67754d717963c0
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-05-05 20:23:36 -0400

    Tests for simple_search

--- vendor/plugins/simple_search/test/simple_search_test.rb
+++ vendor/plugins/simple_search/test/simple_search_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
+$stdout = StringIO.new
+
+def create_properties_table
+  ActiveRecord::Schema.define(:version => 1) do  
+    create_table :properties do |t|  
+      t.column :name, :string  
+      t.column :description, :text  
+    end  
+  end  
+end  
+
+class Property < ActiveRecord::Base  
+  simple_search :name, :description
+end
+
 class SimpleSearchTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    create_properties_table  
+    Property.create(:name => 'Some name', :description => 'Some description')
+    Property.create(:name => 'another name', :description => 'another description')
+  end
+  
+  test "search method is available" do
+    assert Property.respond_to?(:search)
+  end
+
+  test "should search" do
+    assert_equal 2, Property.search("name").size
+    assert_equal 1, Property.search("another").size
+    assert_equal 0, Property.search("swimming").size
   end
 end
