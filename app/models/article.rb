95ea865e3689e372d148bc8e8e36a4bf6a5a55f1
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-05-05 20:02:17 -0400

    Using act_as_taggable_on

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,8 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :categories
   has_many :comments
+  
+  acts_as_taggable
  
   scope :published, where("articles.published_at IS NOT NULL")
   scope :draft, where("articles.published_at IS NULL")
